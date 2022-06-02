89f773d6c4d7eb03e4c869a39380be156e938f66
worashf <uppert83@gmail.com>
2022-06-02 16:53:34 +0300

    add element selectors

---
+++ src/modules/Selector.js
@@ -0,0 +1,5 @@
+const addForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoListWrapper = document.getElementById('todo-list');
+
+export { addForm, todoInput, todoListWrapper };
